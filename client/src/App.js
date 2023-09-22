fa88a417836d61f64f71fc2a60955de91a9df34c
Bhavesh Wadhwani <bhaveshwadhwani21@gmail.com>
2023-09-22 13:22:09 +0530

    chore: updated swagger.json for category and product apis

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ function App() {
           </>
         </ScrollToTop>
         <ToastContainer theme='colored' />
-        <ReactQueryDevtools />
+        {/* <ReactQueryDevtools /> */}
       </QueryClientProvider>
     </>
   );
